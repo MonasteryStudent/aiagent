aaed9c3ed86370e679620200a857c57abde63d41
MonasteryStudent <cs_knoll@proton.me>
2025-07-22 15:16:13 +0200

    update main.py with a generate_content function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import sys
 
 def main():
@@ -11,8 +12,8 @@ def main():
 
     if not args:
         print("AI Code Assistant")
-        print('\nUsage: uv run main.py "prompt here"')
-        print('Example: uv run main.py How to solve TSP in linear time?')
+        print('Usage: uv run main.py "prompt here"')
+        print('Example: uv run main.py Can I use boot.dev to become a better programmer?')
         sys.exit(1)
 
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -23,15 +24,18 @@ def main():
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)])
     ]
+
+    generate_content(client, messages)
         
+def generate_content(client, messages):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001", 
         contents=messages
     )
+    print("Response:")
     print(response.text)
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
     print("Response tokens:", response.usage_metadata.candidates_token_count)
-
 
 if __name__ == "__main__":
     main()
