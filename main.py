801d3683854f1618b985b99df87c9544713c9aa0
MonasteryStudent <cs_knoll@proton.me>
2025-07-22 16:29:19 +0200

    update main.py with command line argument option to make the output verbose

--- main.py
+++ main.py
@@ -7,35 +7,42 @@ import sys
 def main():
 
     load_dotenv()
+    verbose = "--verbose" in sys.argv
 
-    args = sys.argv[1:]
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
 
     if not args:
         print("AI Code Assistant")
-        print('Usage: uv run main.py "prompt here"')
+        print('Usage: uv run main.py "Prompt here."')
         print('Example: uv run main.py Can I use boot.dev to become a better programmer?')
         sys.exit(1)
 
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    
+
     user_prompt = " ".join(args)
+
+    if verbose:
+        print(f"User prompt: {user_prompt}")
 
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)])
     ]
 
-    generate_content(client, messages)
+    generate_content(client, messages, verbose)
         
-def generate_content(client, messages):
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001", 
         contents=messages
     )
-    print("Response:")
-    print(response.text)
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response: \n{response.text}")
 
 if __name__ == "__main__":
     main()
