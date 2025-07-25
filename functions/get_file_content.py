747247977e2b33b5ce3afe1dc1fd6309e5f906a0
MonasteryStudent <cs_knoll@proton.me>
2025-07-25 15:41:49 +0200

    add small_lorem.txt and update get_file_content.py and tests.py to provide tests for get_file_content.py

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,28 +1,28 @@
 import os
-from config import MAX_FILE_SIZE
-
+from functions.config import MAX_FILE_SIZE
 
 def get_file_content(working_directory, file_path):
-    # if file_path is a relative path outside the working_directory, join returns the file_path
+    # if file_path is a relative path outside the working_directory, 
+    # os.path.join() returns file_path
     target_path = os.path.abspath(os.path.join(working_directory, file_path))
     abs_path_working_dir = os.path.abspath(working_directory)
-    print(target_path)
-    print(abs_path_working_dir)
+    # print(target_path)
+    # print(abs_path_working_dir)
     if not target_path.startswith(abs_path_working_dir):
         return f'Error: Cannot read "{target_path}" as it is outside the permitted working directory'
     if not os.path.isfile(target_path):
         return f'Error: File not found or is not a regular file: "{target_path}"'
     try:
         with open(target_path, "r") as f:
-            print(os.path.getsize(target_path))
+            # print(os.path.getsize(target_path))
             if os.path.getsize(target_path) > MAX_FILE_SIZE:
                 file_content_string = f.read(MAX_FILE_SIZE)
-                file_content_string += f'...File "{target_path}" truncatedat {MAX_FILE_SIZE} characters'
+                file_content_string += f'...File "{target_path}" truncated at {MAX_FILE_SIZE} characters'
             else:
                 file_content_string = f.read()
         return file_content_string
     except Exception as e:
-        return f'Error reading file: {e}'
+        return f'Error reading file "{target_path}": {e}'
 
 def test():
     files_content = get_file_content("calculator", "lorem.txt")
--- tests.py
+++ tests.py
@@ -1,16 +1,33 @@
 # tests.py
 
-from functions.get_files_info import get_files_info
+#from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-def main():
-    test_dirs = [".", "pkg", "/bin", "../"]
+def tests():
 
-    for i in range(len(test_dirs)):
-        if test_dirs[i] == ".":
-            print("Result for current directory:")
-        else:
-            print(f"Result for '{test_dirs[i]}' directory:")
-        print(get_files_info("calculator", test_dirs[i]))
+    # - get_files_info tests
+    # test_dirs = [".", "pkg", "/bin", "../"]
+
+    # for i in range(len(test_dirs)):
+    #     if test_dirs[i] == ".":
+    #         print("Result for current directory:")
+    #     else:
+    #         print(f"Result for '{test_dirs[i]}' directory:")
+    #     print(get_files_info("calculator", test_dirs[i]))
+
+    # - get_file_content test
+    # result = get_file_content("calculator", "small_lorem.txt")
+    # print(result)
+
+    # - get_file_content tests
+    file_paths = ["main.py", "pkg/calculator.py", "/bin/cat", "pkg/does_not_exist.py"]
+
+    for file_path in file_paths:
+        file_content = get_file_content("calculator", file_path)
+        print(f"Result for '{file_path}':")
+        print(file_content)
+
+
 
 if __name__ == "__main__":
-    main()
+    tests()
